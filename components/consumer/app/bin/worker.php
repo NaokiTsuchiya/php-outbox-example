df0d48c4bea7218980a0b53bd7c8e851ba86a247
Naoki Tsuchiya <ntsuchiya1105@gmail.com>
2026-03-09 03:47:38 +0900

    takt: wo-shimasu-tasuku-dockerfile-w

--- components/consumer/app/bin/worker.php
+++ components/consumer/app/bin/worker.php
@@ -9,6 +9,10 @@ use MyVendor\OutboxConsumer\Worker\SqsWorker;
 use Psr\Log\LoggerInterface;
 use Swoole\Coroutine;
 
+// STDIO と FILE は Swoole のフック対象から除外する（Swoole 6.x で io_uring が使用不可の環境では
+// フックされたファイル操作が失敗するため、DI コンテナ初期化を保護する）
+Coroutine::set(['hook_flags' => SWOOLE_HOOK_ALL ^ SWOOLE_HOOK_STDIO ^ SWOOLE_HOOK_FILE]);
+
 Coroutine\run(function () {
     $injector = Injector::getInstance('MyVendor\OutboxConsumer', 'app', dirname(__DIR__));
     $logger = $injector->getInstance(LoggerInterface::class);
--- components/pump/app/bin/pump.php
+++ components/pump/app/bin/pump.php
@@ -10,6 +10,9 @@ use Psr\Log\LoggerInterface;
 use Ray\Di\Injector;
 use Swoole\Coroutine;
 
+// STDIO (stderr) は Swoole のフック対象から除外する（Swoole 6.x 以降の互換性）
+Coroutine::set(['hook_flags' => SWOOLE_HOOK_ALL ^ SWOOLE_HOOK_STDIO]);
+
 Coroutine\run(function () {
     $injector = new Injector(new PumpModule());
     $logger = $injector->getInstance(LoggerInterface::class);
